9e19d994f704832816eaac76497805dc5b0bb28e
Walmircsanto <walmirsantosrosa@gmail.com>
2024-07-01 10:46:38 -0300

    :sparkles: feat(ProfessorMapper.java): criado metodo de salva um Professor agora como um DTO

--- wings-up-english-academy/src/main/java/com/wingsupenglishacademy/service/TeacherService.java
+++ wings-up-english-academy/src/main/java/com/wingsupenglishacademy/service/TeacherService.java
@@ -1,11 +1,13 @@
 package com.wingsupenglishacademy.service;
 
+import com.wingsupenglishacademy.DTO.RequestTeacherDTO;
+import com.wingsupenglishacademy.DTO.ResponseTeacherDTO;
+import com.wingsupenglishacademy.mapper.DozerMapper;
+import com.wingsupenglishacademy.mapper.custom.ProfesorMapper;
 import com.wingsupenglishacademy.model.TeacherEntity;
 import com.wingsupenglishacademy.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -15,6 +17,8 @@ public class TeacherService {
     @Autowired
     private TeacherRepository teacherRepository;
 
+    @Autowired
+     private ProfesorMapper mapper;
 
     public TeacherEntity getTeacherById(Long id) {
        return teacherRepository.findById(id).get();
@@ -38,5 +42,17 @@ public class TeacherService {
 
     public void deleteTeacher(Long id) {
         teacherRepository.deleteById(id);
+    }
+
+
+
+    public ResponseTeacherDTO createdTeacher(RequestTeacherDTO teacherDTO) {
+        // converto o DTO para class para pode Salvalo
+       var teacherAux =  DozerMapper.parseObject(teacherDTO, TeacherEntity.class);
+       this.teacherRepository.save(teacherAux);
+
+       return this.mapper.convertToTeacherDTO(teacherAux);
+
+
     }
 }
