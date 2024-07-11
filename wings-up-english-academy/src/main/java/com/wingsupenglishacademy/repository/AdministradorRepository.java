352728bc9f9a72b12157f1d570c405dff0a7808c
Walmircsanto <walmirsantosrosa@gmail.com>
2024-07-11 14:56:45 -0300

    :bug: fix(AdministradorRepository.java): corrigido o bug adicionando na interface AdministradorRepository

--- wings-up-english-academy/src/main/java/com/wingsupenglishacademy/repository/AdministradorRepository.java
+++ wings-up-english-academy/src/main/java/com/wingsupenglishacademy/repository/AdministradorRepository.java
@@ -1,8 +1,9 @@
 package com.wingsupenglishacademy.repository;
 
+import com.wingsupenglishacademy.model.AdministradorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdministradorRepository extends JpaRepository<AdministradorRepository, Long> {
+public interface AdministradorRepository extends JpaRepository<AdministradorEntity, Long> {
 }
