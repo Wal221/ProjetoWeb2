6addf6fd0dcfe7436c0e147439b8933e601f55ee
Walmircsanto <walmirsantosrosa@gmail.com>
2024-07-11 14:49:14 -0300

    :bug: fix(FranquiaRepository.java): corrigido o bug adicionando a anotação Repository na interface

--- wings-up-english-academy/src/main/java/com/wingsupenglishacademy/repository/FranquiaRepository.java
+++ wings-up-english-academy/src/main/java/com/wingsupenglishacademy/repository/FranquiaRepository.java
@@ -2,7 +2,9 @@ package com.wingsupenglishacademy.repository;
 
 import com.wingsupenglishacademy.model.FranquiaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FranquiaRepository extends JpaRepository<FranquiaEntity, Long> {
 
 }
