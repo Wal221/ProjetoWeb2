8fb86ef0aa8cb164034f94b5132bedf1705b5d99
Walmircsanto <walmirsantosrosa@gmail.com>
2024-07-11 14:46:18 -0300

    :bug: fix(AdministradorRepository.java): corrigido o bug adicionando a anotação Repository no na interface

--- wings-up-english-academy/src/main/java/com/wingsupenglishacademy/repository/AdministradorRepository.java
+++ wings-up-english-academy/src/main/java/com/wingsupenglishacademy/repository/AdministradorRepository.java
@@ -1,6 +1,8 @@
 package com.wingsupenglishacademy.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdministradorRepository extends JpaRepository<AdministradorRepository, Long> {
 }
