59156e50cb3e40a3e197338367d075063a721d56
Walmircsanto <walmirsantosrosa@gmail.com>
2024-07-22 10:32:21 -0300

    :sparkles: feat: troca dos atributor por um Enum

--- wings-up-english-academy/src/main/java/com/wingsupenglishacademy/model/AvaliacaoEntity.java
+++ wings-up-english-academy/src/main/java/com/wingsupenglishacademy/model/AvaliacaoEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
 @Entity
 @Table(name = "avaliacao")
@@ -14,9 +15,12 @@ public class AvaliacaoEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Double notas;
+    private Double nota;
+    @Enumerated(EnumType.STRING)
     private TipoAvaliacao tipoAvalicao;
     private Date dataAvalicao;
+    private Double valorAvalicao;
+
 
     public AvaliacaoEntity() {}
 
@@ -30,12 +34,12 @@ public class AvaliacaoEntity implements Serializable {
         this.id = id;
     }
 
-    public Double getNotas() {
-        return notas;
+    public Double getNota() {
+        return nota;
     }
 
-    public void setNotas(Double notas) {
-        this.notas = notas;
+    public void setNota(Double notas) {
+        this.nota = notas;
     }
 
     public TipoAvaliacao getTipoAvalicao() {
@@ -52,5 +56,37 @@ public class AvaliacaoEntity implements Serializable {
 
     public void setDataAvalicao(Date dataAvalicao) {
         this.dataAvalicao = dataAvalicao;
+    }
+
+    public Double getValorAvalicao() {
+        return valorAvalicao;
+    }
+
+    public void setValorAvalicao(Double valorAvalicao) {
+        this.valorAvalicao = valorAvalicao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AvaliacaoEntity that = (AvaliacaoEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(nota, that.nota) && tipoAvalicao == that.tipoAvalicao && Objects.equals(dataAvalicao, that.dataAvalicao) && Objects.equals(valorAvalicao, that.valorAvalicao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nota, tipoAvalicao, dataAvalicao, valorAvalicao);
+    }
+
+    @Override
+    public String toString() {
+        return "AvaliacaoEntity{" +
+                "id=" + id +
+                ", notas=" + nota +
+                ", tipoAvalicao=" + tipoAvalicao +
+                ", dataAvalicao=" + dataAvalicao +
+                ", valorAvalicao=" + valorAvalicao +
+                '}';
     }
 }
