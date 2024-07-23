11602dd5d1bbd63beb31138e301858d3b62c9cee
Walmircsanto <walmirsantosrosa@gmail.com>
2024-07-23 09:53:44 -0300

    :sparkles: feat(AvaliacaoController.java) criado endpoint de created

--- wings-up-english-academy/src/main/java/com/wingsupenglishacademy/controller/AvalicaoController.java
+++ wings-up-english-academy/src/main/java/com/wingsupenglishacademy/controller/AvalicaoController.java
@@ -1,5 +1,27 @@
 package com.wingsupenglishacademy.controller;
 
 
+import com.wingsupenglishacademy.DTO.requests.RequestAvalicaoDTO;
+import com.wingsupenglishacademy.DTO.responses.ResponseAvaliacaDTO;
+import com.wingsupenglishacademy.service.AvaliacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/avaliacao")
 public class AvalicaoController {
+
+    @Autowired
+    AvaliacaoService avaliacaoService;
+
+    @PostMapping(value="/created-avaliacao")
+    public ResponseEntity<ResponseAvaliacaDTO> createdAvalicao(@RequestBody RequestAvalicaoDTO avaliacaoDTO){
+        return new ResponseEntity<>(avaliacaoService.createdAvalicao(avaliacaoDTO), HttpStatus.CREATED);
+
+    }
 }
