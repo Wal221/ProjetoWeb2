c7f01b3d099474b66dbb2aa038732636a365b6a3
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-01 10:46:40 -0300

    :sparkles: Novo metodo de salvar avaliação

--- wings-up-english-academy/src/main/java/com/wingsupenglishacademy/service/AvaliacaoService.java
+++ wings-up-english-academy/src/main/java/com/wingsupenglishacademy/service/AvaliacaoService.java
@@ -4,7 +4,9 @@ import com.wingsupenglishacademy.DTO.requests.RequestAvalicaoDTO;
 import com.wingsupenglishacademy.DTO.responses.ResponseAvaliacaDTO;
 import com.wingsupenglishacademy.mapper.DozerMapper;
 import com.wingsupenglishacademy.model.AvaliacaoEntity;
+import com.wingsupenglishacademy.model.ProfessorEntity;
 import com.wingsupenglishacademy.repository.AvaliacaoRepository;
+import com.wingsupenglishacademy.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,13 +18,23 @@ public class AvaliacaoService {
     @Autowired
     private AvaliacaoRepository avaliacaoRepository;
 
+    @Autowired
+    TeacherRepository teacherRepository;
+
     public List<AvaliacaoEntity> findAll() {return avaliacaoRepository.findAll();}
 
     public ResponseAvaliacaDTO createdAvalicao(RequestAvalicaoDTO avaliacaoDTO) {
         var prova = DozerMapper.parseObject(avaliacaoDTO, AvaliacaoEntity.class);
+        prova.setProfessor(teacherRepository.getReferenceById(avaliacaoDTO.getIdProfesso()));
         avaliacaoRepository.save(prova);
         return DozerMapper.parseObject(prova, ResponseAvaliacaDTO.class);
 
+    }
+
+    public AvaliacaoEntity saveAvaliacaoEntity (AvaliacaoEntity avaliacaoEntity ){
+//        AvaliacaoEntity avalia = avaliacaoEntity;
+//        avalia.setProfessor(professorService.findByTeacherIdEntity(avaliacaoEntity.getProfessor().getId()));
+        return avaliacaoRepository.save(avaliacaoEntity);
     }
 
     public AvaliacaoEntity update(AvaliacaoEntity avaliacaoEntity) { return avaliacaoRepository.save(avaliacaoEntity);}
