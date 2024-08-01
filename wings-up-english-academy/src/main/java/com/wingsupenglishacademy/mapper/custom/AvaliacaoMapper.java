d0b03da21ea80abc86b080020a3b28493b676551
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-01 10:44:26 -0300

    :sparkles: Parse de Avaliação para ResponseDTO

--- wings-up-english-academy/src/main/java/com/wingsupenglishacademy/mapper/custom/AvaliacaoMapper.java
+++ wings-up-english-academy/src/main/java/com/wingsupenglishacademy/mapper/custom/AvaliacaoMapper.java
@@ -1,8 +1,11 @@
 package com.wingsupenglishacademy.mapper.custom;
 
 import com.wingsupenglishacademy.DTO.requests.RequestAvalicaoDTO;
+import com.wingsupenglishacademy.DTO.responses.ResponseAvaliacaDTO;
+import com.wingsupenglishacademy.exceptions.UserNotFoundException;
 import com.wingsupenglishacademy.model.AlunoEntity;
 import com.wingsupenglishacademy.model.AvaliacaoEntity;
+import com.wingsupenglishacademy.model.ProfessorEntity;
 import com.wingsupenglishacademy.service.AlunoService;
 import com.wingsupenglishacademy.service.ProfessorService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,30 +14,41 @@ import org.springframework.stereotype.Service;
 @Service
 public class AvaliacaoMapper {
 
-   @Autowired
-   ProfessorService professorService;
+    @Autowired
+    ProfessorService professorService;
 
-   @Autowired
-   AlunoService alunoService;
+    @Autowired
+    AlunoService alunoService;
 
 
-
-    public AvaliacaoEntity convertToDTOEntity(RequestAvalicaoDTO avalicaoDTO){
+    public AvaliacaoEntity convertToDTOEntity(RequestAvalicaoDTO avalicaoDTO) {
         AvaliacaoEntity avaliacaoEntity = new AvaliacaoEntity();
         avaliacaoEntity.setId(avalicaoDTO.getKey());
         avaliacaoEntity.setDataAvalicao(avalicaoDTO.getDataAvalicao());
         //busco o professor por ID e adiciono como o professor que realizou a prova
-        avaliacaoEntity.setProfessor(professorService.findByTeacherIdEntity(avalicaoDTO.getIdProfesso()));
+        ProfessorEntity professor = new ProfessorEntity();
+        professor.setId(avalicaoDTO.getKey());
+        avaliacaoEntity.setProfessor(professor);
+
         avaliacaoEntity.setValorAvalicao(avalicaoDTO.getValorAvalicao());
         avaliacaoEntity.setTipoAvalicao(avalicaoDTO.getTipoAvalicao());
-        avaliacaoEntity.setNota(avalicaoDTO.getNota());
-        for(int i = 0 ; i < avalicaoDTO.getIdAlunos().size(); i++){
-                                                            //pego os IDs dos alunos eles atraves dos seu Id.
+        for (int i = 0; i < avalicaoDTO.getIdAlunos().size(); i++) {
+            //pego os IDs dos alunos eles atraves dos seu Id.
             AlunoEntity aluno = alunoService.findByIdStudent(avalicaoDTO.getIdAlunos().get(i));
             avaliacaoEntity.getAlunos().add(aluno);
 
         }
         return avaliacaoEntity;
 
+    }
+
+    public ResponseAvaliacaDTO convertEntityToResponse(AvaliacaoEntity avaliacaoEntity){
+        ResponseAvaliacaDTO response = new ResponseAvaliacaDTO();
+
+        response.setDataAvalicao(avaliacaoEntity.getDataAvalicao());
+        response.setValorAvalicao(avaliacaoEntity.getValorAvalicao());
+        response.setTipoAvalicao(avaliacaoEntity.getTipoAvalicao());
+        response.setNameProfessor(avaliacaoEntity.getProfessor().getName());
+        return response;
     }
 }
