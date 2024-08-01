8eaf2a6672f615dda30585aaf824d5198c14f481
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-01 10:43:45 -0300

    :sparkles: Novo metodo de criação de avaliação

--- wings-up-english-academy/src/main/java/com/wingsupenglishacademy/controller/AvalicaoController.java
+++ wings-up-english-academy/src/main/java/com/wingsupenglishacademy/controller/AvalicaoController.java
@@ -3,6 +3,7 @@ package com.wingsupenglishacademy.controller;
 
 import com.wingsupenglishacademy.DTO.requests.RequestAvalicaoDTO;
 import com.wingsupenglishacademy.DTO.responses.ResponseAvaliacaDTO;
+import com.wingsupenglishacademy.model.AvaliacaoEntity;
 import com.wingsupenglishacademy.service.AvaliacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,5 +24,10 @@ public class AvalicaoController {
     public ResponseEntity<ResponseAvaliacaDTO> createdAvalicao(@RequestBody RequestAvalicaoDTO avaliacaoDTO){
         return new ResponseEntity<>(avaliacaoService.createdAvalicao(avaliacaoDTO), HttpStatus.CREATED);
 
+    }
+
+    @PostMapping("/salvar")
+    public ResponseEntity<AvaliacaoEntity> saveAvalicao (@RequestBody AvaliacaoEntity avaliacaoEntity){
+        return new ResponseEntity<>(avaliacaoService.saveAvaliacaoEntity(avaliacaoEntity), HttpStatus.CREATED);
     }
 }
