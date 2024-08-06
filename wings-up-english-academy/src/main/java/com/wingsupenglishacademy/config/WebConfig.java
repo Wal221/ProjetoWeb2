5ab09860a5fd1134d649a964c9806e9bc944312a
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-06 10:37:35 -0300

    :sparkles: Criado Classe de configração de content Negotiation

---
+++ wings-up-english-academy/src/main/java/com/wingsupenglishacademy/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.wingsupenglishacademy.config;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        // Via HeaderParam http/localhost:8080/api/person/v1
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+    }
+}
