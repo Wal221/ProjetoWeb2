2116327767c0e334de0349ccc0d4705a6413925f
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-07 21:23:12 -0300

    :sparkles: feat: Criando controller de documents

--- wings-up-english-academy/src/main/java/com/wingsupenglishacademy/controller/DocumentController.java
+++ wings-up-english-academy/src/main/java/com/wingsupenglishacademy/controller/DocumentController.java
@@ -1,2 +1,33 @@
-package com.wingsupenglishacademy.controller;public class DocumentController {
+package com.wingsupenglishacademy.controller;
+
+import com.wingsupenglishacademy.model.Document;
+import com.wingsupenglishacademy.service.DocumentService;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.time.LocalDateTime;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/documents")
+public class DocumentController {
+
+    private final DocumentService documentService;
+    public DocumentController(DocumentService documentService) {
+        this.documentService = documentService;
+    }
+    @PostMapping("/pdf")
+    public ResponseEntity<String> uploadPdf(@RequestParam("file") MultipartFile file) throws IOException {
+        this.documentService.upload(file);
+        return new ResponseEntity<>( "Pdf salvo com sucesso",HttpStatus.CREATED);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ByteArrayResource> download(@PathVariable Long id) {
+        return documentService.download(id);
+    }
 }
