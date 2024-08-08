69ebe88c4a946229146bc5b31358f1ac0e2a1741
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-07 21:34:57 -0300

    :card_file_box: raw: Alterando a column de Byte

--- wings-up-english-academy/src/main/resources/import.sql
+++ wings-up-english-academy/src/main/resources/import.sql
@@ -2,3 +2,6 @@ INSERT INTO students (grade,id,email,enroll,name,telephone) values(8.8,null,'wal
 INSERT INTO students (id,grade,email,enroll,name,telephone) values( null,8.9, 'Valdir@gmail.com', 2022101,'Valdir','12345678');
 INSERT INTO teacher (id,name,email,telephone,salary,horarioAula,especializacao) values (null, 'Julio', 'julio@email.com', '1111111',   1200.0 , '2021-11-23','Ingles Basico');
 INSERT INTO turma (id,classSchedule, englishLevel,teachingMaterials) values (null,'2023-10-10','avançado','slides');
+
+
+ALTER TABLE document MODIFY COLUMN data LONGBLOB;
