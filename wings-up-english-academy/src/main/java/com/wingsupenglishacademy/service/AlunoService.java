a800d00df572840a48e1e7635fbb6d09d9333d0b
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-10 14:42:19 -0300

    :sparkles: feat: criar metodo de acessar material didatico

--- wings-up-english-academy/src/main/java/com/wingsupenglishacademy/service/AlunoService.java
+++ wings-up-english-academy/src/main/java/com/wingsupenglishacademy/service/AlunoService.java
@@ -4,8 +4,13 @@ import com.wingsupenglishacademy.DTO.requests.RequestAlunoDTO;
 import com.wingsupenglishacademy.DTO.responses.ResponseStudentDTO;
 import com.wingsupenglishacademy.mapper.custom.StudentMapper;
 import com.wingsupenglishacademy.model.AlunoEntity;
+import com.wingsupenglishacademy.model.Document;
+import com.wingsupenglishacademy.repository.DocumentRepository;
 import com.wingsupenglishacademy.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,6 +24,9 @@ public class AlunoService {
 
     @Autowired
     private StudentMapper studentMapper;
+
+    @Autowired
+    private DocumentRepository documentRepository;
 
     public AlunoEntity findByIdStudent(Long id) {
         return studentRepository.findById(id).get();
@@ -41,5 +49,20 @@ public class AlunoService {
 
     public void deleteStudent(AlunoEntity alunoEntity) {
         studentRepository.delete(alunoEntity);
+    }
+
+
+    public ResponseEntity<ByteArrayResource> download(Long id) {
+        Document document = documentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException());
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + document.getName());
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/pdf");
+        headers.add(HttpHeaders.CONTENT_LENGTH, String.valueOf(document.getData().length));
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(new ByteArrayResource(document.getData()));
     }
 }
