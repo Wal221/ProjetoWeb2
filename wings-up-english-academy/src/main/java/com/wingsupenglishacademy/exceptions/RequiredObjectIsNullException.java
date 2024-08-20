60a287e81e5d26e4b61868657d3e47057d8611cd
Walmircsanto <walmirsantosrosa@gmail.com>
2024-08-19 23:49:25 -0300

    :sparkles: feat: adionar exeção de tentar obter Objeto Nulo

--- wings-up-english-academy/src/main/java/com/wingsupenglishacademy/exceptions/RequiredObjectIsNullException.java
+++ wings-up-english-academy/src/main/java/com/wingsupenglishacademy/exceptions/RequiredObjectIsNullException.java
@@ -1,4 +1,4 @@
-package br.com.walmir.restwithspringbootandjava1.exceptions;
+package com.wingsupenglishacademy.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
